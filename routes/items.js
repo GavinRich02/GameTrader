2d783093a5fe87d00cde4b370ba26bf188999497
Gavin Rich <gavintr2002@gmail.com>
2023-01-22 00:46:57 -0600

    Changed routings/allows owner check on item view

--- routes/items.js
+++ routes/items.js
@@ -1,34 +1,51 @@
 const express=require('express');
 const router=express.Router();
 const Item=require('../models/item');
+const User=require('../models/user');
 
 //New
-router.get('/new', (req,res)=> {
-    res.render('./new');
+router.get('/new/:id', async(req,res)=> {
+    res.render('./new', {user: await User.findById(req.params.id)});
 });
 
 //Get all items
 router.get('/user=:id', async (req,res)=> {
     let items=await Item.find();
-    res.render('./index', {items: items, user: user});
+    res.render('./index', {items: items, user: await User.findById(req.params.id)});
 });
 
 //Get single item
-router.get('/:id', async(req,res)=> {
-    res.render('./itemView', {item: await Item.findById(req.params.id)});
+router.get('/:id-:uId', async(req,res)=> {
+    let item=await Item.findById(req.params.id);
+
+    try {
+        if(item.createdBy==req.params.uId) {
+            owner=true;
+        }
+        else {
+            owner=false;
+        }
+    } catch {
+        owner=false;
+    }
+
+    console.log(owner);
+
+    res.render('./itemView', {item: await Item.findById(req.params.id), isOwner: owner});
 });
 
 //Create item
-router.post('/save', async(req,res)=> {
+router.post('/save/:id', async(req,res)=> {
     let item=new Item({
         "name": req.body.name,
         "desc": req.body.desc,
-        "cost": req.body.cost
+        "cost": req.body.cost,
+        "createdBy": req.params.id
     })
 
     await item.save();
 
-    res.redirect('/items/');
+    res.redirect('/items/user='+req.params.id);
 });
 
 //Edit item

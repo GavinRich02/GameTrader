f0f480f4df45b320bd6c916bbdf46d64d35acfa1
Gavin Rich <gavintr2002@gmail.com>
2023-01-23 01:48:40 -0600

    Removed user id from slugs where not necessary

--- routes/items.js
+++ routes/items.js
@@ -3,6 +3,8 @@ const router=express.Router();
 const Item=require('../models/item');
 const User=require('../models/user');
 
+let active;
+
 //New Item
 router.get('/new/:id', async(req,res)=> {
     res.render('./new', {user: await User.findById(req.params.id)});
@@ -10,16 +12,18 @@ router.get('/new/:id', async(req,res)=> {
 
 //Get all items
 router.get('/user=:id', async (req,res)=> {
+    active=req.params.id;
+
     let items=await Item.find();
     res.render('./index', {items: items, user: await User.findById(req.params.id)});
 });
 
 //Get single item
-router.get('/view/:id-:uId', async(req,res)=> {
+router.get('/view/:id', async(req,res)=> {
     let item=await Item.findById(req.params.id);
 
     try {
-        if(item.createdBy==req.params.uId) {
+        if(item.createdBy==String(active)) {
             owner=true;
         }
         else {
@@ -29,7 +33,7 @@ router.get('/view/:id-:uId', async(req,res)=> {
         owner=false;
     }
 
-    res.render('./itemView', {item: await Item.findById(req.params.id), user: await User.findById(req.params.uId), isOwner: owner});
+    res.render('./itemView', {item: await Item.findById(req.params.id), user: await User.findById(active), isOwner: owner});
 });
 
 //Create item
@@ -47,27 +51,27 @@ router.post('/save/:id', async(req,res)=> {
 });
 
 //Edit item
-router.patch('/:id-:uId', async(req,res)=> {
+router.patch('/:id', async(req,res)=> {
     let item=await Item.findById(req.params.id);
     let views=item.views;
 
     views++;
     console.log(views);
 
-    if(item.createdBy!=req.params.uId) {
+    if(item.createdBy!=active) {
         try {
             item.views=views;
             const complete=await item.save();
         } catch {console.log("Oof");};
     }
 
-    res.redirect('/items/view/'+req.params.id+'-'+req.params.uId);
+    res.redirect('/items/view/'+req.params.id);
 });
 
 //Delete item
-router.delete('/:id-:uId', async(req, res)=> {
+router.delete('/:id', async(req, res)=> {
     await Item.findByIdAndDelete(req.params.id);
-    res.redirect('/items/user='+req.params.uId);
+    res.redirect('/items/user='+active);
 });
 
 module.exports=router;
